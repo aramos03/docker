a7fe12cae259a3315456113c96e68592b52334f8
aramos03 <103946743+aramos03@users.noreply.github.com>
2022-10-25 19:59:28 +0200

    index.php

--- index.php
+++ index.php
@@ -1,12 +1,21 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Index</title>
+    <title>Prueba conexión</title>
 </head>
+
 <body>
-    <p>Putón verbenero</p>
+    <?php
+
+    $conn = mysqli_connect("bbdd", "root", "root", "mysql");
+    if ($conn) {
+        echo "Conexión satisfactoria.";
+    }
+    ?>
 </body>
+
 </html>
